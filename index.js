81ba5e9ce171cb005e237c9fab97efb8eed055ba
rmlamarche <rmlamarche@wpi.edu>
2019-11-10 16:04:24 -0500

    ooh thats some smooth scrolling

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  console.log('time to git good!');
+  var navLinks = document.querySelector('nav').querySelectorAll('li > a');
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      var target = this.hash;
+      if (!(typeof target === 'string' && target.length > 1 && target.includes('#'))) {
+        return false;
+      }
+      document.getElementById(target.split('#')[1]).scrollIntoView({behavior:'smooth'});
+    });
+  }
 });
